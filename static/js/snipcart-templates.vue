b62023408eb2f14a1361f78efee295604bf47d1e
Stackbit Code Editor <projects@stackbit.com>
2021-05-01 13:20:21 +0000

    snipcart-templates.vue: changed

--- static/js/snipcart-templates.vue
+++ static/js/snipcart-templates.vue
@@ -1,1 +1,1 @@
-<div hidden id="snipcart" data-api-key="__SNIPCART_API_KEY__"></div>
+<div hidden id="snipcart" data-api-key=MzcxYWVkYTgtNWE4Yi00YTNmLWE2ZTctZjAyNWI4MGJjOTA1NjM3NTQyNTY5NzYyNDAwMDQ1"></div>
